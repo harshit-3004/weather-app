8f1b3adb92da561c6652b7887fa49d0ea5fa1702
harshit-3004 <h.chaubisa30@gmail.com>
2022-05-08 22:58:30 +0530

    final touch up

--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import '../src/Header.css'
 
 class Header extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            }
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
   render() {
     return (
       <div className='headerbg'>
-     <span className='heading' style={{color: 'white' , fontSize: '34px'}}>Weather App</span>
+        <span className='heading'>Weather App</span>
       </div>
     );
   }
